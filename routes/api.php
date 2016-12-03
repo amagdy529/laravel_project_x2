ac68f9fc652c8b6ca23f8f34ef98a20949aa0741
amagdy529 <amagdy529@gmail.com>
2016-12-03 15:47:39 +0200

    shit

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:api');
 
 Route::get('/test', 'visitsApiController2@index');
+
+
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::resource('users', 'UserController');
 
 
 Route::resource('visits', 'visitController');
+
